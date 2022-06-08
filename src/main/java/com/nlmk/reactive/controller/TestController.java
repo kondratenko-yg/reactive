15df2aa03d32b12c86cea8ad584f92a85f553d01
kondratenko_yg <kondratenko_yg@nlmk.com>
2022-06-08 14:24:00 +0300

    Controller + logs

---
+++ src/main/java/com/nlmk/reactive/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.nlmk.reactive.controller;
+
+import com.nlmk.reactive.domain.Country;
+import com.nlmk.reactive.service.TestService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RestController
+@RequestMapping("/test")
+@AllArgsConstructor
+public class TestController {
+    private final TestService service;
+    @GetMapping
+    public Flux<Country> test(){
+        return service.findHot()
+                .onErrorResume(t -> {log.error("Error", t); return Mono.empty();})
+                .switchIfEmpty(Mono.defer(Mono::empty));
+    }
+}
--- src/test/java/com/nlmk/reactive/service/TestServiceImplTest.java
+++ src/test/java/com/nlmk/reactive/service/TestServiceImplTest.java
@@ -2,12 +2,15 @@ package com.nlmk.reactive.service;
 
 import com.nlmk.reactive.domain.Country;
 import com.nlmk.reactive.repository.TestRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+@Slf4j
 @SpringBootTest
 class TestServiceImplTest {
     private TestService testService;
@@ -24,7 +27,7 @@ class TestServiceImplTest {
     void test(){
         var expected = new Country().setId(3L).setName("Brazil").setHot(true);
         StepVerifier
-                .create(testService.findHot())
+                .create(testService.findHot().onErrorResume(t -> {log.error("Error", t); return Mono.empty();}))
                 .expectNext(expected)
                 .expectComplete()
                 .verify();
